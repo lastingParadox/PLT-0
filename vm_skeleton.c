cc05ad811e256bb9931281f6d7930da7dad72260
lastingParadox <zanpres@gmail.com>
2022-09-22 23:01:26 -0400

    Initial commit

---
+++ vm_skeleton.c
@@ -0,0 +1,83 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "compiler.h"
+
+int base(int *stack, int BP, int L);
+void print_instruction(int PC, instruction IR);
+void print_stack(int PC, int BP, int SP, int *stack);
+
+void execute(int trace_flag, instruction *code)
+{
+	if (trace_flag)
+	{
+		printf("VM Exectution:\n");
+		printf("\t\t\t\tPC\tBP\tSP\tstack\n");
+		printf("Initial Values:\t\t\t0\t0\t-1\n");
+	}
+}
+
+int base(int *stack, int BP, int L)
+{
+	while (L > 0)
+	{
+		BP = stack[BP];
+		L--;
+	}
+	return BP;
+}
+
+
+void print_stack(int PC, int BP, int SP, int *stack)
+{
+	int i;
+	printf("%d\t%d\t%d\t", PC, BP, SP);
+	for (i = 0; i <= SP; i++)
+		printf("%d ", stack[i]);
+	printf("\n");
+}
+
+void print_instruction(int PC, instruction IR)
+{
+	char opname[4];
+	
+	switch (IR.op)
+	{
+		case LIT : strcpy(opname, "LIT"); break;
+		case OPR :
+			switch (IR.m)
+			{
+				case ADD : strcpy(opname, "ADD"); break;
+				case SUB : strcpy(opname, "SUB"); break;
+				case MUL : strcpy(opname, "MUL"); break;
+				case DIV : strcpy(opname, "DIV"); break;
+				case EQL : strcpy(opname, "EQL"); break;
+				case NEQ : strcpy(opname, "NEQ"); break;
+				case LSS : strcpy(opname, "LSS"); break;
+				case LEQ : strcpy(opname, "LEQ"); break;
+				case GTR : strcpy(opname, "GTR"); break;
+				case GEQ : strcpy(opname, "GEQ"); break;
+				default : strcpy(opname, "err"); break;
+			}
+			break;
+		case LOD : strcpy(opname, "LOD"); break;
+		case STO : strcpy(opname, "STO"); break;
+		case CAL : strcpy(opname, "CAL"); break;
+		case RTN : strcpy(opname, "RTN"); break;
+		case INC : strcpy(opname, "INC"); break;
+		case JMP : strcpy(opname, "JMP"); break;
+		case JPC : strcpy(opname, "JPC"); break;
+		case SYS : 
+			switch (IR.m)
+			{
+				case WRT : strcpy(opname, "WRT"); break;
+				case RED : strcpy(opname, "RED"); break;
+				case HLT : strcpy(opname, "HLT"); break;
+				default : strcpy(opname, "err"); break;
+			}
+			break;
+		default : strcpy(opname, "err"); break;
+	}
+	
+	printf("%d\t%s\t%d\t%d\t", PC - 1, opname, IR.l, IR.m);
+}
